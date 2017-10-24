e5ff9c0847cf9ddccd3999e8072f8cd993445eb3
BoRenChen <bo0920065577@gmail.com>
2017-10-23 23:49:29 -0400

    Throttle implementation

--- lib/microblog_web/controllers/session_controller.ex
+++ lib/microblog_web/controllers/session_controller.ex
@@ -24,7 +24,7 @@ defmodule MicroblogWeb.SessionController do
     |> put_session(:user_id, nil)
     |> put_flash(:info, "Logged out")
     |> redirect(to: status_path(conn, :index))
-  
+  end
     # COPY FROM NAT's NOTES
     # TODO: Move to user.ex
   def get_and_auth_user(email, password) do
@@ -41,4 +41,31 @@ defmodule MicroblogWeb.SessionController do
   # ...
       |> put_flash(:error, "Bad email/password")
  end
+  def update_tries(throttle, prev) do
+    if throttle do
+      prev + 1
+    else
+      1
+    end
+  end
+
+  def throttle_attempts(user) do
+    y2k = DateTime.from_naive!(~N[2000-01-01 00:00:00], "Etc/UTC")
+    prv = DateTime.to_unix(user.pw_last_try || y2k)
+    now = DateTime.to_unix(DateTime.utc_now())
+    thr = (now - prv) < 3600
+
+    if (thr && user.pw_tries > 5) do
+      nil
+    else
+      changes = %{
+          pw_tries: update_tries(thr, user.pw_tries),
+          pw_last_try: DateTime.utc_now(),
+      }
+      IO.inspect(user)
+      {:ok, user} = Ecto.Changeset.cast(user, changes, [:pw_tries, :pw_last_try])
+      |> NuMart.Repo.update
+      user
+    end
+  end
 end
